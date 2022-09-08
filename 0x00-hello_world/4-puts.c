484dbdd1347315a8e0cd0c8ac3ae95f31e04fe6b
Uheezal <aleemabdulmuheez@gmail.com>
2022-09-08 15:15:28 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main - printing using put
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
+ *
  */
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
