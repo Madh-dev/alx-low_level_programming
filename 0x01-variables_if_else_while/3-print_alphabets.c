f0563ad78702c7a8eb6f28ca3056c0455b30082b
Uheezal <aleemabdulmuheez@gmail.com>
2022-09-09 18:11:42 -0700

    missing blank line declaration cleared

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,17 @@ int main(void)
 {
 	char lower = 'a';
 	char upper = 'A';
-	while (lower <= 'z' )
+
+	while (lower <= 'z')
 	{
 		putchar(lower);
 		lower++;
 	}
-	while (upper <= 'Z' )
+	while (upper <= 'Z')
 	{
 		putchar(upper);
 		upper++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
